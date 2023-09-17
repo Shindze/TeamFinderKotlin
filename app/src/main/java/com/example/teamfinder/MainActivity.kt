b824c8045e5275eaeff5c91f8c3d0693f8b84fe5
Koviello Arlekkino <vainglory.ru@mail.ru>
2023-09-17 15:07:22 +0300

    ADD: simple fragment

---
+++ app/src/main/java/com/example/teamfinder/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.teamfinder
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.teamfinder.ui.theme.TeamFinderTheme
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TeamFinderTheme {
+                Scaffold(
+                    modifier = Modifier
+                        .background(color = MaterialTheme.colorScheme.background)
+                        .size(size = 300.dp)
+                ) {
+                    Greeting("Lesya")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!", modifier = modifier
+    )
+}
